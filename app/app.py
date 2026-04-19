8b066a0d47387032e23ee51ba4e77b273cdeb1ed
Deebyendu Mondal <48420567+Deebyendu@users.noreply.github.com>
2026-04-19 15:10:15 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,3 @@
-import sys
-import os
-
-# Point Python to the root directory of your project
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 from pipeline.pipeline import AnimeRecommendationPipeline
 from dotenv import load_dotenv
